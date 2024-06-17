5805d5f44ba8642e0f8c73f5ed9a91c43fe7ec98
farinaleo <leo.farina.fr@gmail.com>
2024-06-17 10:06:40 +0200

    :recycle: edit code to fit with the readme

--- srcs/computePartialDerivative.py
+++ srcs/computePartialDerivative.py
@@ -2,32 +2,30 @@ import pandas as pd
 from .estimatedPrice import estimated_price
 
 
-def compute_partial_derivative_0(df: pd.DataFrame, thetas: tuple, learning_rate: float = 0.5) -> float:
+def compute_partial_derivative_0(df: pd.DataFrame, thetas: tuple) -> float:
     """
     Apply the given formula from the subject to compute the actual gradiant (g0).
     The given formula can be associate to a partial derivative (see the README.MD).
     :param df: Dataframe containing the subject data (normalised).
     :param thetas: Tuple of temporary thetas to improve.
-    :param learning_rate: Learning rate.
     :return: Gradient 0.
     """
     tmp_sum = 0
     for _, row in df.iterrows():
         tmp_sum += estimated_price(row['km'], thetas=thetas) - row['price']
-    return learning_rate / len(df) * tmp_sum
+    return 1 / len(df) * tmp_sum
 
 
-def compute_partial_derivative_1(df: pd.DataFrame, thetas: tuple, learning_rate: float = 0.5) -> float:
+def compute_partial_derivative_1(df: pd.DataFrame, thetas: tuple) -> float:
     """
         Apply the given formula from the subject to compute the actual gradiant (g1).
     The given formula can be associate to a partial derivative (see the README.MD).
 
         :param df: Dataframe containing the subject data (normalised).
         :param thetas: Tuple of temporary thetas to improve.
-        :param learning_rate: Learning rate.
         :return: Gradient 1.
         """
     tmp_sum = 0
     for _, row in df.iterrows():
         tmp_sum += (estimated_price(row['km'], thetas=thetas) - row['price']) * row['km']
-    return learning_rate / len(df) * tmp_sum
+    return 1 / len(df) * tmp_sum
